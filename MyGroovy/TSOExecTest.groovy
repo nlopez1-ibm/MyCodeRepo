5ec4da79aafe743b50d410d8a36708597180be80
nelson <62708912+nlopez1-ibm@users.noreply.github.com>
2024-01-22 10:20:47 -0500

    Update TSOExecTest.groovy

--- MyGroovy/TSOExecTest.groovy
+++ MyGroovy/TSOExecTest.groovy
@@ -1,41 +1,39 @@
-/* DBB Groovy Class to demo how to convert a file with Rexx using DBB's TSOExec  api (NLOPEZ)   
+I wrote this groovy script for a customer to do some pre-processing of a cobol pgm using Rexx sample that I also wrote. If you need a walk thru let me know.  They can adapt that to run their Rexx.
+
+```/* DBB Groovy Class to demo how to convert a file with Rexx using DBB's TSOExec  api (NLOPEZ)   
  *
- *  runs a rexx exec to convert cobol cldoe with changeman "-INC  xxx" in col 1 to normal cobol "COPY xxx" in col 8
+ *  runs a rexx exec to convert cobol code with changeman "-INC  xxx" in col 1 to normal cobol "COPY xxx" in col 8
  *  uses 2 pds's and a rexx exec on MVS
  *  PDS DD=in  source with potential -INC
  *  PDS DD=out converted members PDS (prealloacted on 3.2) 
  *  RC=0 nothing found 
  *  RC=2 conversions made and output to DD=out      
- *  In zDT run groovyz2 $PWD/this_script    cd to the right place first for pwd to work
- *  this is the non auto daemon becuase it grapb the wrong userid!!
  */   
 import com.ibm.dbb.build.* 
 
 TSOExec exec = new TSOExec()
-	//exec.setCommand("ex 'NLOPEZ.DAT.EXEC(CMMIG)'")
-	exec.setCommand("listalc")
+	exec.setCommand("ex 'NLOPEZ.DAT.EXEC(CMMIG)'")	
 	exec.confDir('/u/nlopez/dbb-110/conf')					// points to the runIspf.sh interface 
 	
 	exec.logFile(new File('/u/nlopez/tmp/TSOExec.log'))  	// has log fo ispf gateway interface	
 	exec.keepCommandScript(true)  							// does not delete the file below. Goo for tracing errors
     exec.addDDStatement("CMDSCP", "NLOPEZ.ISPFGWY.EXEC", "RECFM(F,B) LRECL(80) TRACKS SPACE(1,1) DSORG(PS)", false);
 
- 	//exec.addDDStatement("IN",  "NLOPEZ.DAT.COBOL(CMINC)", "SHR", false);
- 	//exec.addDDStatement("OUT", "NLOPEZ.DAT.COBOL2(CMINC)", "SHR", false);
+ 	exec.addDDStatement("IN",  "NLOPEZ.DAT.COBOL(CMINC)", "SHR", false);
+ 	exec.addDDStatement("OUT", "NLOPEZ.DAT.COBOL2(CMINC)", "SHR", false);
  
 	int rc = exec.execute()
-			
-//if (rc == 2) println "**! Member Converted. See OUT DD PDS"
+        if (rc == 2) println "**! Member Converted. See OUT DD PDS"
 	
 println "RC = $rc"
 System.exit(rc)
 	 
  
  
-/* Sample rexx code to be copied to a pds om MVS and used above
-*   need to all the enclosing slash asterisk to the comments and remove the leading asterisks
+/* Sample rexx code to be copied to a pds on MVS and used above
+*  To use the rexx below, enclose above slash asterisk and unconments the code below 
 *   
-*      
+* the code ... as a comment    
 * REXX TO MIGRATE CM CBL FILE WITH -INC  (NLOPEZ) `                
 * SAVE; TSO EX 'NLOPEZ.DAT.EXEC(CMMIG)'                            
 * look for "-INC" in col 1 and convert to standard cobol copy stmt 
@@ -73,4 +71,4 @@ System.exit(rc)
 * end                                                
 * exit 0                                             
 */
- 
+  ```
